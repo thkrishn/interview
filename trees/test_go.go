94f6f4d787361f11ec39f017fa7e443c5678b93e
tejas Krishna <tejashkrishna@gmail.com>
2017-07-19 15:32:20 -0500

    spacing

--- trees/test_go.go
+++ trees/test_go.go
@@ -5,9 +5,11 @@ import "fmt"
 func main() {
     queue := make([]int, 0)
     // Push
+
     queue = append(queue, 1)
     // Top (just get next element, don't remove it)
     x := queue[0]
+
     // Discard top element
     fmt.Println(x)
 
