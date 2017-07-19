9f2cc13bb4e3e96b97b87738ec82a520605fd8a1
tejas Krishna <tejashkrishna@gmail.com>
2017-07-19 15:25:40 -0500

    test

--- trees/test_go.go
+++ trees/test_go.go
@@ -2,18 +2,18 @@ package main
 
 import "fmt"
 
-func main(){
-queue := make([]int, 0)
-// Push
-queue = append(queue, 1)
-// Top (just get next element, don't remove it)
-x := queue[0]
-// Discard top element
-fmt.Println(x)
+func main() {
+    queue := make([]int, 0)
+    // Push
+    queue = append(queue, 1)
+    // Top (just get next element, don't remove it)
+    x := queue[0]
+    // Discard top element
+    fmt.Println(x)
 
-queue = queue[1:]
-// Is empty ?
-if len(queue) == 0 {
-    fmt.Println("Queue is empty !")
-}
+    queue = queue[1:]
+    // Is empty ?
+    if len(queue) == 0 {
+        fmt.Println("Queue is empty !")
+    }
 }
