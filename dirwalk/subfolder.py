0af2a0cbd8e9bf75ec16fdee2ac379da41641bc9
tejas <tejashkrishna@gmail.com>
2017-07-30 18:27:56 -0500

    adding a new file

---
+++ dirwalk/subfolder.py
@@ -0,0 +1,19 @@
+import os
+import sys 
+
+def subfolder(path):
+	print path
+	#lst = os.listdir(path)
+	for i in os.listdir(path):
+		childpath = os.path.join(path,i)
+		if (os.path.isdir(childpath)):
+			print childpath,
+			subfolder(childpath)
+			
+
+
+
+cwd = os.getcwd()
+print sys.argv[1]
+
+subfolder(cwd)
