30a6c357749a8a055026c81f37ff00b1801e12f6
thkrishn <tejashkrishna@gmail.com>
2019-08-10 14:11:43 -0700

    update

--- dirwalk/subfolder.py
+++ dirwalk/subfolder.py
@@ -2,18 +2,18 @@ import os
 import sys 
 
 def subfolder(path):
-	print path
-	#lst = os.listdir(path)
-	for i in os.listdir(path):
-		childpath = os.path.join(path,i)
-		if (os.path.isdir(childpath)):
-			print childpath,
-			subfolder(childpath)
+    print(path)
+    #lst = os.listdir(path)
+    for i in os.listdir(path):
+        childpath = os.path.join(path,i)
+        if (os.path.isdir(childpath)):
+            print (childpath)
+            subfolder(childpath)
+        
+        print(i)
 			
 
-
-
 cwd = os.getcwd()
-print sys.argv[1]
+print(sys.argv[1])
 
 subfolder(cwd)
